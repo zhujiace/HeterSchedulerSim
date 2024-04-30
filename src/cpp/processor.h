ce07fe3bd222ed75f6dc1c0d94d62a441b92eeca
hamham223 <niyinchen@sjtu.edu.cn>
2024-04-30 19:43:36 +0800

    feat: first commit on processor settings

---
+++ src/cpp/processor.h
@@ -0,0 +1,60 @@
+/*
+Copy Right. The EHPCL Authors.
+*/
+
+#ifndef PROCESSOR_H
+#define PROCESSOR_H
+
+#include <string>
+
+/**
+ * @brief Describes task affinity on which types of processors,. 
+*/
+enum ProcessorAffinity_t {
+    CPU,
+    CPUBigCore,
+    CPULittleCore,
+    DataCopy,
+    DataCopyHTD,
+    DataCopyDTH,
+    PE,
+    GPU,
+    FPGA,
+    UNKNOWN
+};
+
+typedef ProcessorAffinity_t ProcessorType_t;
+
+const std::string ProcessorAffinityNames[10] = {
+    "CPU",
+    "CPUBigCore",
+    "CPULittleCore",
+    "DataCopy",
+    "DataCopyHTD",
+    "DataCopyDTH",
+    "PE",
+    "GPU",
+    "FPGA",
+    "UNKNOWN",
+};
+
+enum ProcessorState_t {
+    IDLE,
+    BUSY,
+    DEAD,
+    NONPREEMPTIVE,
+    UNKNOWN
+};
+
+class Processor {
+
+    ProcessorType_t processorType; 
+    ProcessorState_t processorState;
+
+public:
+    ProcessorState_t stateQuery();
+    ProcessorType_t typeQuery();
+    
+};
+
+#endif // processor.h
